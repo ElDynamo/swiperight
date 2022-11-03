e14bbdd7ed98d7bc551fdc4d965fa90b0043b26d
ElDynamo <theDynamo2001@gmail.com>
2022-11-03 22:38:14 +0100

    Zmiany

---
+++ src/components/SimpleGallery.vue
@@ -0,0 +1,138 @@
+<script>
+	import baguetteBox from "baguettebox.js";
+	window.addEventListener("load", function () {
+		baguetteBox.run(".gallery", {
+			// fullScreen: true,
+		});
+	});
+</script>
+
+<template>
+	<h1 class="">GALERIA ZDJĘĆ</h1>
+	<div class="gallery grid grid-cols-6 gap-2 py-20">
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="First image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+		<a href="../../resources/hobby-1.jpg">
+			<img
+				src="../../resources/hobby-1.jpg"
+				alt="Second image" />
+		</a>
+	</div>
+</template>
+
+<style>
+	@import "baguettebox.js/dist/baguetteBox.min.css";
+</style>
